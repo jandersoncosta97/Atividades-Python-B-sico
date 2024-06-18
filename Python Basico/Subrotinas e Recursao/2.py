b3e2c0d3da7c5843ec7f4b2034ed31ff6ff5c475
Janderson Lima Costa <167913806+jandersoncosta97@users.noreply.github.com>
2024-06-18 17:06:21 -0300

    Versão #9

--- Python Basico/Subrotinas e Recursao/2.py
+++ Python Basico/Subrotinas e Recursao/2.py
@@ -20,37 +20,40 @@ while num > 100:
         num = int(input("N = "))
 """ 
     
-
-def separa(num):
+def separa(num1):
     l = []
-    while num > 0:
-        v = num % 10
-        num = num // 10
-        l.append(v)
-        print("Desmembrando: ", num, l)
+    while num1 > 0:
+        v = num1 % 10
+        num1 = num1 // 10
+        l.insert(0, v)
+        print("Desmembrando: ", num1, l)
     return l
 
 
-def verificacao(nu):
-    listas = []
+def juntar(lista):
+    a = 1
+    v = 0
+    for i in lista:
+        v += i*(10**(len(lista)-a))
+        a += 1
+    return v
+
+def listar_numeros(nu):
     numero = 0
+    numeros = []
     for i in range(100, nu+1, 1):
         lista = separa(i)
-        lista_invertida = lista[::-1]
-        if lista_invertida == lista:
-            a = 0
-            for e in lista:
-                numero += e*(len(lista)-a)
-                a += 1
-            listas.append(numero)
-    return listas
+        if lista == lista[::-1]:
+            numero = juntar(lista)
+            numeros.append(numero)
+    return numeros
 
 n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
 while n > 99999:
     n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
 while n > 0:
-    res = verificacao(n)
-    print(n, res)
+    res = listar_numeros(n)
+    print("Lista palíndromos entre 100 e", n, ":" ,res)
     n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
     while n > 99999:
         n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
