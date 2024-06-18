f05059b4705970555bd4aa4be8c623be0483d5fc
Janderson Lima Costa <167913806+jandersoncosta97@users.noreply.github.com>
2024-06-18 16:14:57 -0300

    Versão #8

--- Python Basico/Subrotinas e Recursao/2.py
+++ Python Basico/Subrotinas e Recursao/2.py
@@ -20,28 +20,36 @@ while num > 100:
         num = int(input("N = "))
 """ 
     
+
 def separa(num):
     l = []
     while num > 0:
         v = num % 10
         num = num // 10
-        l.insert(0, v)
+        l.append(v)
         print("Desmembrando: ", num, l)
     return l
 
-def contagem(nu, numero = 9):
-    li = separa(nu)
-    r = 0
-    for e in li:
-        if e == numero:
-            r = r + 1
-    return r
+
+def verificacao(nu):
+    listas = []
+    numero = 0
+    for i in range(100, nu+1, 1):
+        lista = separa(i)
+        lista_invertida = lista[::-1]
+        if lista_invertida == lista:
+            a = 0
+            for e in lista:
+                numero += e*(len(lista)-a)
+                a += 1
+            listas.append(numero)
+    return listas
 
 n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
 while n > 99999:
     n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
 while n > 0:
-    res = contagem(n)
+    res = verificacao(n)
     print(n, res)
     n = int(input("Digite um número inteiro positivo (0 < num < 99999): "))
     while n > 99999:
